77085768ed5a5bfb6d3035392867595a4e0d7e1e
jepretarim1814 <tarim.jepre@gmail.com>
2019-11-22 15:33:50 +0800

    able to create post with dynamic categories

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -11,7 +11,7 @@
                 </div>
                 <div class="form-group">
                     {!! Form::label('category_id', 'Category:') !!}
-                    {!! Form::select('category_id', ['' => 'Choose Category', 1 => 'PHP', 2 => 'Javascript'], null, ['class' => 'form-control']) !!}
+                    {!! Form::select('category_id', ['' => 'Choose Category'] + $categories, null, ['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('photo_id', 'Photo:') !!}
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -22,7 +22,7 @@
                 <td>{{$post->id}}</td>
                 <td><img style="height: 50px; width: 50px;" src="{{$post->photo ? $post->photo->file : 'https://via.placeholder.com/50'}}" alt=""></td>
                 <td>{{$post->user->name}}</td>
-                <td>{{$post->category_id}}</td>
+                <td>{{$post->category ? $post->category->name : 'Un-categorized'}}</td>
                 <td>{{$post->title}}</td>
                 <td>{{$post->body}}</td>
                 <td>{{$post->created_at}}</td>
